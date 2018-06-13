8381910aa792ca265bc72d657b5e948234f0205c
le.gutierrez <leo342135@gmail.com>
2018-06-13 18:21:57 -0500

    Changes to text in welcome page

--- src/components/user/Profile.jsx
+++ src/components/user/Profile.jsx
@@ -20,9 +20,33 @@ class Profile extends Component {
 
     render() {
         return (
-            <div>
+            <div className="container text-center">
                 <Image src="assets/small-header.jpg" className="header-image" />
                 <h1>Welcome! Thank you for registering.</h1>
+                <p>You have just been reborn into Sensorium...</p>
+                <p>The psycellium is working hard to find your cluster. Be patient, the reward will be amazing.</p>
+                {/*<p>In the mean time, we will appreciate if you share this with your friends and family.</p>
+                <div className="container text-center">
+                    <div className="a2a_kit a2a_kit_size_32 a2a_default_style">
+                        <a className="a2a_dd" href="https://www.addtoany.com/share"></a>
+                        <a className="a2a_button_facebook hidden-xs"></a>
+                        <a className="a2a_button_twitter hidden-xs"></a>
+                        <a className="a2a_button_google_plus hidden-xs"></a>
+                        <a className="a2a_button_tumblr hidden-xs "></a>
+                        <a className="a2a_button_reddit hidden-xs "></a>
+                        <a className="a2a_button_whatsapp hidden-xs "></a>
+                        <a className="a2a_button_google_gmail hidden-xs "></a>
+                        <a className="a2a_button_digg hidden-xs "></a>
+                        <a className="a2a_button_facebook_messenger hidden-xs "></a>
+                        <a className="a2a_button_kik hidden-xs "></a>
+                        <a className="a2a_button_myspace hidden-xs "></a>
+                        <a className="a2a_button_viber hidden-xs "></a>
+                        <a className="a2a_button_wechat hidden-xs "></a>
+                        <a className="a2a_button_yahoo_messenger hidden-xs "></a>
+                        <a className="a2a_button_aol_mail hidden-xs "></a>
+                    </div>
+                </div>*/}
+                <br />
                 <a className="btn btn-grad-peach" onClick={this.goBack.bind(this)}>Go back to home page</a>
             </div>
         )
