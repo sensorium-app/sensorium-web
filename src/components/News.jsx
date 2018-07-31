a9c80916a7094e36d21995deedeb0e0ba59da120
Ilyas <41810227+Of-Elysium@users.noreply.github.com>
2018-07-31 05:11:37 -0500

    Add files via upload
    Updated JSX files with CSS to match

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -12,11 +12,10 @@ export default class News extends Component {
           <h2>News</h2>
           <Row>
             <Col xs={12} sm={8} className="main-section">
-              <h3>June 9th 2018</h3>
-              <p>These days have been tough for us. The await for the Sense8 special episode 'Amor Vincit Omnia' and preparing to launch this site with few sleep.</p>
-              <p>But we're finally here with no regrets because it has been and we know it will be worth it. 
-                Something by fans, for the fans. The Sensorium Social Network is now on its pre-registration phase! Add you name, email, birthdate and very soon you will have the opportunity to share a new experience on the web based on Netflix's Original Series Sense8.</p>
-              <small>Disclaimer: This is a fan generated project and is not officialy affiliated to Netflix, nor we pretend to infringe any copyright material.</small>
+              <h3>June 9th, 2018</h3>
+              <p>These days have been tough for us.</p>
+              <p>But we're finally here with no regrets because we know it will be worth it. 
+                Something by the fans, for the fans. The Sensorium Social Network is now in its pre-registration phase! Add your name, email, and birthday and you will have the opportunity to share a new experience based on Netflix's original series Sense8.</p>
             </Col>
             <Col xs={12} sm={4} className="sidebar-section">
               <Image src="assets/team.jpg" />
