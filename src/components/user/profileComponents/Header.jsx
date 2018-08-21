7055d7db8310a56125c2c3d91c7ec626bc1739e1
Leonardo Gutiérrez Hernández <leo342135@gmail.com>
2018-08-21 06:56:40 -0500

    Fix logout button onClick function

--- src/components/user/profileComponents/Header.jsx
+++ src/components/user/profileComponents/Header.jsx
@@ -61,7 +61,7 @@ class Header extends Component {
                     <a className="profile-btn-grad"><span className="lnr lnr-user"></span></a>
                     <a className="profile-btn-grad"><span className="lnr lnr-users"></span></a>
                     <a className="profile-btn-grad"><span className="lnr lnr-cog"></span> </a>
-                    <a className="profile-btn-grad" onClick="this.logout.bind(this)"><span className="lnr lnr-power-switch"></span></a>
+                    <a className="profile-btn-grad" onClick={this.logout.bind(this)}><span className="lnr lnr-power-switch"></span></a>
 
                 </div>
                 <div className="cluster-name">
