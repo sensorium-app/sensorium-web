dd0986c3424bc716bf7033a063574a8733b5c190
Prateek <prateekgupta1198@gmail.com>
2018-11-01 00:32:24 +0530

    comment and like button added to  post footer

--- src/components/user/profileComponents/post-ui/PFooter.jsx
+++ src/components/user/profileComponents/post-ui/PFooter.jsx
@@ -1,12 +1,23 @@
 import React, { Component } from "react";
 import PropTypes from 'prop-types';
+import{Row} from 'reactstrap';
 import { withRouter } from 'react-router-dom';
 import styles from './styles';
-
+import './style/post.css';
 const PFooter = (props) =>(
     <div className="Post-caption">
          <p style={styles.postCaption}>{props.postcaption}</p>
+
+        <div className="Post-console">
+            <i class="fa fa-reply"></i>
+            <i class="fa fa-heart"></i>
+        </div>
+        
     </div>
+    
+     
+    
+    
 
     )
 
--- src/components/user/profileComponents/post-ui/PHeader.jsx
+++ src/components/user/profileComponents/post-ui/PHeader.jsx
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
-
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import styles from './styles';
 
 
 const PHeader = (props) =>(
       <div className="header">
+        <div className="settings-dropdown">
+       
+        </div>
         <span className="account-group" style={styles.accountGroup}>
           <img className="avatar" src={props.image} style={styles.avatar} />
           <strong className="fullname" style={styles.fullname}>{props.name}</strong>
           <p><strong><span style={styles.timestamp}>{props.timestamp}</span></strong></p>
         </span>
-          
       </div>
     )
 
